0ca56f571d4e4cdc8cf331175e0e74bd6187c649
Rosa Turner <rmturner1220@gmail.com>
2022-07-06 11:02:26 -0400

    updted

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -42,47 +43,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+ 
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -91,6 +56,25 @@ app.get('/api/animals', (req, res) => {
       res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+    res.json(result);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+
+
+
+
